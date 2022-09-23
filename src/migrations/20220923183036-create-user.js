ed6d1f7613193b0b8ee4d728774eb37ff9a9d261
Isabel Terenzi <bel.terenzi@gmail.com>
2022-09-23 16:21:56 -0300

    requisito 1

---
+++ src/migrations/20220923183036-create-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.createTable('users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+
+    displayName: {
+      field: 'display_name',
+      type: Sequelize.STRING
+    },
+
+    email: {
+      unique: true,
+      type: Sequelize.STRING
+    },
+
+    password: {
+      type: Sequelize.STRING
+    },
+
+    image: {
+      type: Sequelize.STRING
+    }
+   })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
